a21f0abdd6d460693ceb199126dfeeff17977356
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2023-10-11 11:45:57 +0300

    done first task

---
+++ index.js
@@ -0,0 +1,1 @@
+import "./tasks/task-1.js";
---
+++ tasks/task-1.js
@@ -0,0 +1,56 @@
+// It's Friday night, and Chuck is bored. He's already run 1,000 miles,
+// stopping only to eat a family sized bag of Heatwave Doritos and a large
+// fistful of M&Ms. He just can't stop thinking about kicking something!
+
+// There is only one thing for it, Chuck heads down to his local MMA gym
+// and immediately challenges every fighter there to get in the cage and try
+// and take him down... AT THE SAME TIME!
+
+// You are provided an array of strings that represent the cage and Chuck's opponents.
+// Your task, in traditional Chuck style, is to take their heads off!! Throw punches, kicks, headbutts (or more likely - regex or iteration...) but whatever you do, remove their heads. Return the same array of strings, but with the heads ('O') removed and replaced with a space (' ').
+
+// If the provided array is empty, or is an empty string, return 'Gym is empty'.
+// If you are given an array of numbers, return 'This isn't the gym!!'.
+
+// FIGHT!!
+
+// Original design of this kata was a much more beautiful thing - the test
+// cases illustrate the idea, and the intended output. I am unable to make
+// the actual output go over multiple lines so for now at least you will have
+// to imagine the beauty!
+
+function headSmash(array) {
+  if (array.length === 0 || array === "") {
+    return "Gym is empty";
+  }
+  if (array.some((el) => typeof el === "number")) {
+    return "This is not a gym!!";
+  }
+  const res = array.map((el) => {
+    const toArray = el.split("");
+    const result = toArray.map((el) => {
+      if (el === "O") {
+        return el.replace(el, " ");
+      }
+      return el;
+    });
+    return result.join("");
+  });
+  return res;
+}
+
+console.log(
+  headSmash([
+    "*****************************************",
+    "***********   _O_   *   _O_   ***********",
+    "**  _O_   *  C(.)J  *  /(.)J  *   _O_  **",
+    "** /(.)J  *  _| |_  *  _/ )_  *  C(.)J **",
+    "** _/ )_  *********************  _/ |_ **",
+    "******************* X *******************",
+    "**********************  _O_  ************",
+    "**  _O_   *   _O_   *  /(.)J  *   _O_  **",
+    "** /(.)J  *  C(.)J  *  _/ )_  *  C(.)J **",
+    "** _( )_  *  _| |_  ***********  _/ |_ **",
+    "****************************************",
+  ])
+);
